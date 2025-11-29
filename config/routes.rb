abafed5a4d9a4d3c034ebb9385543c211e2fac10
Eric Laquer <LaquerEric@gmail.com>
2025-11-29 09:09:00 -0500

    Bump parent version to 0.1.11. Bump child version to 0.1.21. update scripts

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
 ActiveDataFlow::Engine.routes.draw do
-  # Routes for ActiveDataFlow management interface
-  root to: "dashboard#index"
-  
-  resources :data_flows, only: [:index, :show] do
-    member do
-      post :trigger
-    end
-    
-    resources :data_flow_runs, only: [:index, :show, :create]
-  end
 end
